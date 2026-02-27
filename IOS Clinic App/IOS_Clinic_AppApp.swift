15899330f445c8c78a8b2ecdc82ddfe3bda6a662
chamuditha01 <heshanchamuditha05@gmail.com>
2026-02-27 20:31:43 +0530

    First Commit

---
+++ IOS Clinic App/IOS_Clinic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Clinic_AppApp.swift
+//  IOS Clinic App
+//
+//  Created by Dileesha on 2026-02-27.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Clinic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
